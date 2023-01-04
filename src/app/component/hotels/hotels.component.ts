b8de1f9c5243cdbd9393a2c406592286e4e08201
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-01-04 11:37:52 +0000

    done with components

--- src/app/component/hotels/hotels.component.ts
+++ src/app/component/hotels/hotels.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./hotels.component.css']
 })
 export class HotelsComponent {
+  // get the data from the service
+  constructor() { }
+
+  // get the data from the service
+  ngOnInit() {
+    // caliing the endpoint to retrieve the data from the database
+    
+  }
 
 }
