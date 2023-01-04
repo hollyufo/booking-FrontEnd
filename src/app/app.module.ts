14a4f0f4d873e27c5285d35c939afe318b259ac6
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-01-04 09:10:11 +0000

    created Header and hotel components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HotelsComponent } from './component/hotels/hotels.component';
+
+// Routes
+const AppRoutes: Routes = [
+  { path: 'hotels', component: HotelsComponent },
+  { path: '', redirectTo: '/hotels', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HotelsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
