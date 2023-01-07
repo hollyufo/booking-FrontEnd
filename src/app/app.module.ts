70238b86f1077c7f44885ccee321dcc067f33221
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-01-07 22:24:12 +0100

    created the header and hero section components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HerosectionComponent } from './components/herosection/herosection.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HerosectionComponent
+    HerosectionComponent,
+    HomeComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
