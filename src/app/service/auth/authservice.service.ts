e247721dcf73d228afa34c4abba92cb4e8d87b22
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-01-11 21:31:36 +0100

    added guard role

--- src/app/service/auth/authservice.service.ts
+++ src/app/service/auth/authservice.service.ts
@@ -1,15 +1,25 @@
+
+
 import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthserviceService {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   // save token to local storage
   saveToken(token: string) {
     localStorage.setItem('token', token);
+    // decode token
+    const decodedToken = jwtDecode(token);
+
+    console.log(decodedToken); // Outputs the payload of the JWT as a JavaScript object
   }
   // get token from local storage
   getToken() {
